b17bc939dc364d1dd37c23c01bc2ff1e56fcedbf
FujiwaLaTeX <13024156@mmm.muroran-it.ac.jp>
2018-02-02 18:13:43 +0900

    コメントを消した

--- scrape_article.py
+++ scrape_article.py
@@ -7,35 +7,16 @@ import os
 from time import sleep
 import socket
 from selenium.webdriver.chrome.options import Options
-"""
-cssselecters = {
-    "dousoku.net": "div.color:#0000cd",
-    "ssbiyori.blog.fc2.com": "div.entry_htext",
-    "142ch.blog90.fc2.com": "dd.t_b",
-    "tangaron3.sakura.ne.jp/nobel": "p",
-    "horahorazoon.blog134.fc2.com": "b",
-    "sssokuhou.com": "div.t_b",
-    "amnesiataizen.blog.fc2.com":"div.t_b",
-    "blog.livedoor.jp/mode_ss": "b"
-    }
-"""
+
 options = Options()
 options.binary_location = "/usr/bin/google-chrome-stable"
 options.add_argument("--headless")
 
 driver = webdriver.Chrome(chrome_options=options)
-# driver.implicitly_wait(120)
-# driver.set_page_load_timeout(120)
-
 
 def get_article(url):
     driver.get(url)
     root = lxml.html.fromstring(driver.page_source)
-    # domain = re.search(r"http://(.+)/").group(1)
-    # law_articles = root.cssselect('div.t_b') + \
-    # root.cssselect('p') + \
-    # root.cssselect('b') + \
-    # root.cssselect('div.ently_text')
     law_articles = root.cssselect("div")
     if len(law_articles) == 0:
         with open("articles/problem_urls.csv", "a") as f:
@@ -61,12 +42,8 @@ def get_many_article():
                 with open("articles/problem_urls.dat", "a") as f:
                     f.write(url + "\n")
                     print(f"problem occured in {url}")
-                # for sec in range(1, 7200+1):
-                    # print(f"{7200-sec}")
-                    # sleep(1)
                 break
             except TimeoutException as e:
-                print("ほぎゃーーーーーー！！！！！！")
                 print(e)
                 continue
             else:
